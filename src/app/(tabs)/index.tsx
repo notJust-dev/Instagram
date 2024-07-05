7d996b7587f6e3bba38c7a841c76d4881e68f9f1
Vadim Savin <savinvadim1312@gmail.com>
2024-07-05 17:58:59 +0300

    Responsive Feed

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -6,8 +6,9 @@ export default function FeedScreen() {
   return (
     <FlatList
       data={posts}
+      className="items-center"
       renderItem={({ item }) => <PostListItem post={item} />}
-      contentContainerStyle={{ gap: 10 }}
+      contentContainerStyle={{ gap: 10, maxWidth: 512, width: '100%' }}
       showsVerticalScrollIndicator={false}
     />
   );
