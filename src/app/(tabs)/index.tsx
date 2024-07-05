606d1fd3798f4721ba74adaf8dd89b355ae852a0
Vadim Savin <savinvadim1312@gmail.com>
2024-07-05 18:22:12 +0300

    Create post UI

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -6,9 +6,13 @@ export default function FeedScreen() {
   return (
     <FlatList
       data={posts}
-      className="items-center"
       renderItem={({ item }) => <PostListItem post={item} />}
-      contentContainerStyle={{ gap: 10, maxWidth: 512, width: '100%' }}
+      contentContainerStyle={{
+        gap: 10,
+        maxWidth: 512,
+        alignSelf: 'center',
+        width: '100%',
+      }}
       showsVerticalScrollIndicator={false}
     />
   );
--- src/app/(tabs)/new.tsx
+++ src/app/(tabs)/new.tsx
@@ -1,5 +1,37 @@
-import { Text } from 'react-native';
+import { Text, View, Image, TextInput, Pressable } from 'react-native';
+import { useState } from 'react';
 
 export default function CreatePost() {
-  return <Text>Create post</Text>;
+  const [caption, setCaption] = useState('');
+
+  return (
+    <View className="p-3 items-center flex-1">
+      {/* Image picker */}
+      <Image
+        source={{
+          uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/1.jpg',
+        }}
+        className="w-52 aspect-[3/4] rounded-lg bg-slate-300"
+      />
+
+      <Text onPress={() => {}} className="text-blue-500 font-semibold m-5">
+        Change
+      </Text>
+
+      {/* TextInput for caption */}
+      <TextInput
+        value={caption}
+        onChangeText={(newValue) => setCaption(newValue)}
+        placeholder="What is on your mind"
+        className="w-full p-3"
+      />
+
+      {/* Button */}
+      <View className="mt-auto w-full">
+        <Pressable className="bg-blue-500 w-full p-3 items-center rounded-md">
+          <Text className="text-white font-semibold">Share</Text>
+        </Pressable>
+      </View>
+    </View>
+  );
 }
