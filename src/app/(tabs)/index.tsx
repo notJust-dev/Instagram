edf24140c3e46439204ae88e04b2cf9e209d7466
Vadim Savin <savinvadim1312@gmail.com>
2024-07-12 19:06:21 +0300

    Fetch post like with the post

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -19,9 +19,11 @@ export default function FeedScreen() {
     setLoading(true);
     let { data, error } = await supabase
       .from('posts')
-      .select('*, user:profiles(*)')
-      // .eq('user_id', user?.id) // show only my posts
+      .select('*, user:profiles(*), my_likes:likes(*)')
+      // .eq('id', 49) // show only my posts
+      .eq('my_likes.user_id', user.id)
       .order('created_at', { ascending: false });
+
     if (error) {
       Alert.alert('Something went wrong');
     }
--- src/components/PostListItem.tsx
+++ src/components/PostListItem.tsx
@@ -26,8 +26,11 @@ export default function PostListItem({ post }) {
   const { user } = useAuth();
 
   useEffect(() => {
-    fetchLike();
-  }, []);
+    if (post.my_likes.length > 0) {
+      setLikeRecord(post.my_likes[0]);
+      setIsLiked(true);
+    }
+  }, [post.my_likes]);
 
   useEffect(() => {
     if (isLiked) {
@@ -37,19 +40,19 @@ export default function PostListItem({ post }) {
     }
   }, [isLiked]);
 
-  const fetchLike = async () => {
-    const { data, error } = await supabase
-      .from('likes')
-      .select('*')
-      .eq('user_id', user?.id)
-      .eq('post_id', post.id)
-      .select();
+  // const fetchLike = async () => {
+  //   const { data, error } = await supabase
+  //     .from('likes')
+  //     .select('*')
+  //     .eq('user_id', user?.id)
+  //     .eq('post_id', post.id)
+  //     .select();
 
-    if (data && data?.length > 0) {
-      setLikeRecord(data[0]);
-      setIsLiked(true);
-    }
-  };
+  //   if (data && data?.length > 0) {
+  //     setLikeRecord(data[0]);
+  //     setIsLiked(true);
+  //   }
+  // };
 
   const saveLike = async () => {
     if (likeRecord) {
@@ -108,6 +111,16 @@ export default function PostListItem({ post }) {
         <Feather name="send" size={20} />
 
         <Feather name="bookmark" size={20} className="ml-auto" />
+      </View>
+
+      <View className="px-3 gap-1">
+        <Text className="font-semibold">58 likes</Text>
+        <Text>
+          <Text className="font-semibold">
+            {post.user.username || 'New user'}{' '}
+          </Text>
+          {post.caption}
+        </Text>
       </View>
     </View>
   );
